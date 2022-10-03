c0080b43b18055482b87d02f77735a7d017db12c
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-10-03 18:19:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
 // next.config.js
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    images: {
-      unoptimized: true
-    }
-  }
-}
-
-const withMDX = require('@next/mdx')(nextConfig)
+const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
-  // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    unoptimized: true
+  },
   webpack(config, options) {
     return config
   }
