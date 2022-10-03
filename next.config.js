0a69e0600ae62f99125bef6588d11f99260b0c93
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-10-03 18:15:30 +0200

    test next dependencies

--- next.config.js
+++ next.config.js
@@ -13,6 +13,8 @@ const nextConfig = {
 const withMDX = require('@next/mdx')(nextConfig)
 
 module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   webpack(config, options) {
     return config
   }
