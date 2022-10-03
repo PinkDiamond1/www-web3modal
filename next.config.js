d2a484db75f0741d4a618fa0c3649130a31cef4f
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-10-03 17:30:02 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,18 @@ const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
+    experimental: {
+      images: {
+        allowFutureImage: true,
+        unoptimized: true
+      }
+    }
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
-  },
+  }
 })
+
 module.exports = withMDX({
   // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
 })
