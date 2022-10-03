9248ca7a90624d95814103874dffe92f4face260
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-10-03 17:19:44 +0200

    Update for deployment

--- src/components/Community/Index.tsx
+++ src/components/Community/Index.tsx
@@ -1,22 +1,22 @@
-import Card from "../Card"
-import Text from "../Text/Index"
-import Image from "next/image"
-import FEATURE_CARDS from "../../data/FEATURE_CARDS"
-import Button from "../Button/Index"
+import Card from '../Card'
+import Text from '../Text/Index'
+import Image from 'next/image'
+import FEATURE_CARDS from '../../data/FEATURE_CARDS'
+import Button from '../Button/Index'
 
 export default function Community() {
   const { title, image, text } = FEATURE_CARDS[9]
 
   return (
     <Card color="dark" padding="none" rounded="lg">
-      <div style={{ padding: "2rem" }}>
+      <div style={{ padding: '2rem' }}>
         <Text variant="heading4" color="white">
           {title}
-        </Text>{" "}
+        </Text>
         <Text variant="text2" color="grey">
           {text}
         </Text>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: 'flex' }}>
           <Button variant="fill" color="grey">
             Github
           </Button>
