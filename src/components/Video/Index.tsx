212f27534cedc0c2739b55eacd9771afedb54d82
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-10-24 20:35:11 +0200

    test commit

--- src/components/Video/Index.tsx
+++ src/components/Video/Index.tsx
@@ -3,7 +3,26 @@ import { isMobile } from '../../utils/Index'
 import s from './styles.module.css'
 
 export default function Video() {
-  const mobile = isMobile()
+  const [width, setWidth] = useState(0) // default width, detect on server.
+  // let WINDOW_WIDTH
+  // let WINDOW_HEIGHT
+  // const handleResize = () => setWidth(window.innerWidth)
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     setWidth(window.innerWidth)
+  //     console.log('width', width)
+  //     if (width > 1000 && width < 1500) {
+  //       setWidth(1200)
+  //     }
+  //     if (width > 1000 && width < 1500) {
+  //       setWidth(1200)
+  //     }
+  //     // detect window screen width function
+  //   }
+  //   // window.addEventListener('resize', handleResize)
+  //   // return () => window.removeEventListener('resize', handleResize)
+  // }, [width])
 
   return (
     <div className={s.videoWrapper}>
@@ -12,8 +31,8 @@ export default function Video() {
         autoPlay
         loop
         //ToDo: figure out...
-        width={mobile ? 500 : '100%'}
-        height={mobile ? 600 : 562}
+        width={isMobile() ? window.innerWidth : 1000}
+        height={isMobile() ? window.innerWidth : 562}
       >
         <source src="/web3modal.mp4" type="video/mp4" />
       </video>
