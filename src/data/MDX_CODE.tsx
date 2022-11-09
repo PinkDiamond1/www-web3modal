f8239b1b4ce8a64bbef1b0dd9d13dbbdad6077a9
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-11-09 13:17:43 -0700

    tom fb

--- src/data/MDX_CODE.tsx
+++ src/data/MDX_CODE.tsx
@@ -38,14 +38,14 @@ export default function HomePage() {
 
 //-------- TRY IT OUT -------------------------------//
 export const TRY_IT_OUT_CODE = `
-import { useConnected, ConnectButton, useConnectModal } from '@web3modal/react'
+import { useAccount, ConnectButton, useConnectModal } from '@web3modal/react'
 export default function YourAppContent() {
   const { isOpen, open, close } = useConnectModal()
-  const { connected } = useConnected()
+  const { account } = useAccount()
 
   return (
     <>
-      <div>{!isConnected ? <ConnectButton /> : accountButton()}</div>
+      <div>{!account.isConnected ? <ConnectButton /> : accountButton()}</div>
       {/* or */}
       <button onClick={open}>Open Modal</button>
     </>
